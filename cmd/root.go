fb0ff6fc1b14a3d09c55c1fb6db8d43dda69715d
Brad Soper <brad.soper@run.ai>
2024-09-24 12:39:54 -0700

    updated root command for logger

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,24 @@
 /*
 Copyright © 2024 brad.soper@run.ai
-
 */
 package cmd
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
+var (
+	InfoLogger    *log.Logger
+	WarningLogger *log.Logger
+	ErrorLogger   *log.Logger
+)
 
 // rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
+var RootCmd = &cobra.Command{
 	Use:   "runctl",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
@@ -30,7 +35,7 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
+	err := RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
@@ -41,11 +46,44 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
+	// Persistent flag to define the namespace
+	RootCmd.PersistentFlags().StringP("namespace", "n", "default", "If present, the namespace scope for this CLI request")
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.runctl.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	err := setLogger("")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
+// setLogger sets up the logger to write to a file
+// p is the file name to write to
+func setLogger(p string) error {
+	// Set the log file path
+	var fileName = p
 
+	// If no file name is provided, use the default runctl-logs.txt
+	if fileName == "" {
+		fileName = "runctl-logs.txt"
+	}
+
+	// Open the log file
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+		return fmt.Errorf("there was an issue creating the log file. %v", err)
+	}
+
+	// Set the logger to write to the file
+	log.SetOutput(file)
+
+	// Set up the loggers
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	return nil
+}
